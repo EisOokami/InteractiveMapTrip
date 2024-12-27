f69e128beecef2f37ff2bfe04cbd4df7c8d5a5fe
LakioLive <mr.lakio.sergii@gmail.com>
2024-12-27 18:30:54 +0100

    refactor: Updated layout and styling for loading components

--- src/components/ui/loadings/loadingTrip/LoadingTrip.tsx
+++ src/components/ui/loadings/loadingTrip/LoadingTrip.tsx
@@ -3,7 +3,7 @@ import "react-loading-skeleton/dist/skeleton.css";
 
 export default function LoadingTrip() {
     return (
-        <div className="grid content-center gap-4 p-3 border-r">
+        <div className="absolute grid content-start gap-4 p-3 w-screen md:w-97 h-full bg-white dark:bg-dark-mode-black md:border-r z-[1999]">
             <div className="w-full">
                 <Skeleton width="140px" height="40px" />
             </div>
@@ -18,19 +18,21 @@ export default function LoadingTrip() {
                     <Skeleton width="100%" height="40px" />
                 </div>
             </div>
-            <div className="w-full px-5">
-                <Skeleton width="100px" height="40px" />
-            </div>
-            <div className="grid gap-2 px-5">
+            <div className="trip__cards grid gap-2 px-5 overflow-x-hidden overflow-y-scroll">
                 <div className="w-full">
-                    <Skeleton width="100%" height="200px" />
+                    <Skeleton width="100px" height="40px" />
+                </div>
+                <div className="grid gap-2">
+                    <div className="w-full">
+                        <Skeleton width="100%" height="200px" />
+                    </div>
+                    <div className="w-full">
+                        <Skeleton width="100%" height="200px" />
+                    </div>
                 </div>
                 <div className="w-full">
-                    <Skeleton width="100%" height="200px" />
+                    <Skeleton width="150px" height="50px" />
                 </div>
-            </div>
-            <div className="w-full px-5">
-                <Skeleton width="150px" height="50px" />
             </div>
         </div>
     );
